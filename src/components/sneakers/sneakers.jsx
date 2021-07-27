c422fd356f7e8b89034383e05c8145ec684e161b
Chanwooong2 <mcw0219@gmail.com>
2021-07-27 22:04:38 +0900

    View 완료

--- src/components/sneakers/sneakers.jsx
+++ src/components/sneakers/sneakers.jsx
@@ -12,10 +12,10 @@ const Sneakers = ({ sneakers }) => {
 			<div className={styles.releaseDate}>{sneakers.drawDate}<span>{sneakers.drawDay}</span></div>	
 			<div className={styles.theme}>{sneakers.theme}</div>	
 			<div className={styles.img}>
-				<img src={sneakers.imgUrl} alt={sneakers.theme + " loading..."} loading="lazy"/>
+				<img src={sneakers.imgUrl} alt={sneakers.theme + " loading..."} onClick={()=>{openDrawPage(sneakers.drawPageUrl)}} loading="lazy"/>
 			</div>
-			<div className={styles.title}>{sneakers.title}</div>	
-			<div className={styles.detailTime}>{sneakers.dataInfo}<br/>{sneakers.dataInfo}</div>	
+			<div className={`${styles.title} ${styles.ellipsis}`}>{sneakers.title}</div>
+			<div className={styles.detailTime}>{sneakers.dateInfo[0]}<br/>{sneakers.dateInfo[1]}</div>	
 			<button className={styles.button} onClick={()=>{openDrawPage(sneakers.drawPageUrl)}} title="Go! ✨Nike✨"></button>
 		</div>
 	);
