1fc53755581bdf93f9711dd1f9f4dc862397da2a
Chanwooong2 <mcw0219@gmail.com>
2021-08-01 22:20:35 +0900

    메일 전송 및 스케쥴러 기능 완료

---
+++ src/service/mailDataHandling.js
@@ -0,0 +1,62 @@
+getMailContents =()=> {
+    const sneakers = getSneakersJsonData();
+    const todaySneakers = getTodaySneakersList(sneakers);
+    const mailQueue = getMailQueue(todaySneakers);
+
+    return mailQueue;
+}
+
+getSneakersJsonData =()=> {
+    const fs = require('fs');
+    const data = fs.readFileSync('../data/sneakersInformation.json', 'utf8').slice(9);
+    const sneakers = JSON.parse(data);
+
+    // 시간순 정렬
+    sneakers.sort((a, b) => (new Date(a.drawYear+"/"+a.drawDate+" "+a.drawStartTime)) - (new Date(b.drawYear+"/"+b.drawDate+" "+b.drawStartTime)));
+
+    return sneakers;
+}
+
+// 오늘 드로우 대상 리스트
+getTodaySneakersList =(sneakers)=> {
+    const date = new Date();
+    let today;
+    
+    // Issue : GitHub Actions의 스케쥴러가 제 시간에 돌지 않고 0 ~ 2시간30분 정도 딜레이 발생하는 현상
+    // '0 22 * * *' 로 설정 > 한국시간 기준 오전 7시 예정
+    // 세계시간 기준 딜레이 포함해서 22, 23시에 실행 시 날짜 하루 추가
+    if(date.getHours() > 21){
+        // 9시 이전에 돌았을 경우 (날짜가 하루 전날이라 안걸렷을 것)
+        today = date.getMonth()+1+"/"+(date.getDate()+1);
+    }else{
+        // 10시 이후에 돌았을 경우 (10시에 드로우하는 것이 안 걸림)
+        today = date.getMonth()+1+"/"+date.getDate();
+    }
+    const todaySneakers = sneakers.filter(item => item.drawDate === today);
+
+    console.log(today);
+    return todaySneakers;
+}
+
+getMailQueue =(todaySneakers)=> {
+    const mailQueue = [];
+    let criteria = todaySneakers.length !== 0 ? todaySneakers[0].drawStartTime : "";
+    let tmpArr = [];
+    console.log(`criteria : ${criteria}`);
+    for(let i=0; i< todaySneakers.length; i++){
+        if(criteria !== todaySneakers[i].drawStartTime){
+            mailQueue.push({criteria:criteria, sneakersList:tmpArr});
+            criteria = todaySneakers[i].drawStartTime;
+            tmpArr = [];
+        }
+        tmpArr.push(todaySneakers[i]);
+        if(i === todaySneakers.length-1){
+            mailQueue.push({criteria:criteria, sneakersList:tmpArr});
+        }
+    }
+    
+    console.log(mailQueue);
+    return mailQueue;
+}
+
+module.exports = {getMailContents};
---
+++ src/service/mailManager.js
@@ -0,0 +1,75 @@
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
+const dataHandling = require('./mailDataHandling.js');
+
+const transporter = nodemailer.createTransport(smtpTransport({
+	service : "gmail", 
+	host : "smtp.gmail.com",
+	auth :{
+		user : "mcw0219Dev@gmail.com",
+		pass : process.env.mail_pass
+	}
+}));
+
+const getMailData =()=> {
+    let mailHtml = "";
+
+    mailHtml += "<div style=\"width: 100%;\">";
+    mailHtml += "	<div style=\"text-align: center;font-size: 1.3em;font-weight: bold;padding:10px;cursor:pointer;border-radius: 30px;margin: 5px auto;background: #f1b848;width: 260px;position: absolute;left: calc(50% - 130px);\"><a style=\"text-decoration: none;color:#291a60;\" href=\"https://chanwooong2.github.io/nike-sneakers/nike-sneakers\">Go Chanwoong's<br>👟Sneakers Page👟</a></div>";
+    mailHtml += "	<div style=\"text-align: center;font-size:2em;font-weight: bold;padding:10px;cursor:pointer;border-radius: 30px;margin: 5px auto;background: #000;color: #fff;width: 260px;line-height: 40px;position: absolute;left: calc(50% - 130px);top: 95px;\"><a style=\"text-decoration: none;color:#fff;\" href=\"https://www.nike.com/kr/launch/?type=upcoming&activeDate=date-filter:AFTER_DATE\">Go Nike.com</a></div>";
+    mailHtml += "</div>";
+
+    return mailHtml;
+}
+
+const getReceiverList =()=> {
+	const receiverFile = require('fs');
+    const data = receiverFile.readFileSync('../data/receiverInfo.json', 'utf8');
+    const receiverInfo = JSON.parse(data);
+	let result = receiverInfo.addressList.join(", ");
+	
+	console.log(`receiverList : [${result}]`);
+	return result;
+}
+
+const mailOptions = {
+	from : 'mcw0219Dev@gmail.com',
+	to : getReceiverList(),
+	subject : 'Nike The Draw!',
+	html : getMailData()
+};
+
+// 시간을 cron 표기로 변환하는 함수
+// 9시간 계산
+const timeToCronExpression =(timeStr)=> {
+	let hour = Number(timeStr.split(":")[0]);
+	const minute = Number(timeStr.split(":")[1]);
+	
+	hour = hour < 9 ? hour - 9 + 24 : hour = hour - 9;
+
+	return minute+ " " +hour+" * * *";
+}
+
+const mailQueue = dataHandling.getMailContents();
+
+const cron = require('node-cron');
+let mailTask;
+let taskList = [];
+console.log(`mailQueue : ${mailQueue}`);
+for(let i=0; i < mailQueue.length; i++){
+	let schedule = timeToCronExpression(mailQueue[i].criteria);
+	console.log(`schedule : ${schedule}`);
+	mailTask = cron.schedule(schedule, () =>{
+		transporter.sendMail(mailOptions, function(err, info){
+			if(err){
+				console.log(err);
+			}else{
+				console.log(`Success! ${info.response}`);
+			}
+			console.log(taskList[0])
+			taskList[0].stop();
+			taskList.splice(0,1);
+		});
+	});
+	taskList.push(mailTask);
+}
